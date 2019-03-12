218b426a899b4195403f421211702bfefe184015
louieogrady <l.ogrady@gmail.com>
2019-03-12 17:32:20 +0000

    fin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -8,6 +7,55 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  # get '/' do
+  # end
+
+  get '/articles/new' do
+    erb :new
   end
+
+  # get '/show' do
+  #   erb :show
+  # end
+  #
+  # get '/index' do
+  #   erb :index
+  # end
+
+  get '/articles' do
+    @articles = Article.all
+    #binding.pry
+    erb :index
+  end
+
+  post '/articles' do
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect to "/articles/#{@article.id}"
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save
+    redirect to "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id/delete' do
+    @article = Article.find_by_id(params[:id])
+    @article.delete
+    redirect to '/articles'
+  end
+
 end
